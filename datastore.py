d3880bc455d08754dbe4fe11ce2e1cadb2fafe1e
yc5424tl <yc5424tl@go.minneapolis.edu>
2018-01-21 22:45:35 -0600

    Resolves Issue #4
    Book data is now stored as JSON.
    
    Accordingly, the data is also deserialized when read.

--- datastore.py
+++ datastore.py
@@ -1,5 +1,6 @@
-
+# coding=utf-8
 import os
+import json
 from book import Book
 
 DATA_DIR = 'data'
@@ -11,19 +12,18 @@ separator = '^^^'  # a string probably not in any valid data relating to a book
 book_list = []
 counter = 0
 
+
 def setup():
-    ''' Read book info from file, if file exists. '''
+    """ Read book info from file, if file exists. """
 
     global counter
 
-    try :
-        with open(BOOKS_FILE_NAME) as f:
-            data = f.read()
-            make_book_list(data)
+    try:
+        with open(BOOKS_FILE_NAME) as json_file_to_read:
+            make_book_list(json.load(json_file_to_read))
     except FileNotFoundError:
         # First time program has run. Assume no books.
         pass
-
 
     try:
         with open(COUNTER_FILE_NAME) as f:
@@ -36,25 +36,23 @@ def setup():
 
 
 def shutdown():
-    '''Save all data to a file - one for books, one for the current counter value, for persistent storage'''
-
-    output_data = make_output_data()
+    """Save all data to a file - one for books, one for the current counter value, for persistent storage"""
 
     # Create data directory
     try:
         os.mkdir(DATA_DIR)
     except FileExistsError:
-        pass # Ignore - if directory exists, don't need to do anything. 
+        pass  # Ignore - if directory exists, don't need to do anything.
 
-    with open(BOOKS_FILE_NAME, 'w') as f:
-        f.write(output_data)
+    with open(BOOKS_FILE_NAME, 'w') as target_output_file:
+        json.dump(make_output_data(), target_output_file)
 
     with open(COUNTER_FILE_NAME, 'w') as f:
         f.write(str(counter))
 
 
 def get_books(**kwargs):
-    ''' Return books from data store. With no arguments, returns everything. '''
+    """ Return books from data store. With no arguments, returns everything. """
 
     global book_list
 
@@ -62,13 +60,12 @@ def get_books(**kwargs):
         return book_list
 
     if 'read' in kwargs:
-        read_books = [ book for book in book_list if book.read == kwargs['read'] ]
+        read_books = [book for book in book_list if book.read == kwargs['read']]
         return read_books
 
 
-
 def add_book(book):
-    ''' Add to db, set id value, return Book'''
+    """ Add to db, set id value, return Book"""
 
     global book_list
 
@@ -83,7 +80,7 @@ def generate_id():
 
 
 def set_read(book_id, read):
-    '''Update book with given book_id to read. Return True if book is found in DB and update is made, False otherwise.'''
+    """Update book with given book_id to read. Return True if book is found in DB and update is made, False otherwise."""
 
     global book_list
 
@@ -93,35 +90,37 @@ def set_read(book_id, read):
             book.read = True
             return True
 
-    return False # return False if book id is not found
+    return False  # return False if book id is not found
+
+    # TODO - this can just return book.read
 
 
-
-def make_book_list(string_from_file):
-    ''' turn the string from the file into a list of Book objects'''
+def make_book_list(data):
+    """ turn the string from the file into a list of Book objects"""
 
     global book_list
 
-    books_str = string_from_file.split('\n')
-
-    for book_str in books_str:
-        data = book_str.split(separator)
-        book = Book(data[0], data[1], data[2] == 'True', int(data[3]))
-        book_list.append(book)
+    for title in data:
+        book_list.append(Book(title, data.get(title.get('author')), data.get(title).get('id'), data.get(title.get('read'))))
 
 
 def make_output_data():
-    ''' create a string containing all data on books, for writing to output file'''
+    """ create a string containing all data on books, for writing to output file"""
 
     global book_list
 
-    output_data = []
+    data_to_output_dict = {}
 
     for book in book_list:
-        output = [ book.title, book.author, str(book.read), str(book.id) ]
-        output_str = separator.join(output)
-        output_data.append(output_str)
+        data_to_output_dict[book.title] = dict(author=book.author, id=book.id, read=book.read)
 
-    all_books_string = '\n'.join(output_data)
+    return data_to_output_dict
 
-    return all_books_string
+
+def test_read_json_load_data():
+    # check json dump
+
+    with open(BOOKS_FILE_NAME) as json_file_to_read:
+        input_data_dict = json.load(json_file_to_read)
+        for book_dict in input_data_dict:
+            print(book_dict.keys())
