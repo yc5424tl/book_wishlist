83be166500a1172c35e5e07f074086716844785d
yc5424tl <yc5424tl@go.minneapolis.edu>
2018-01-23 06:07:52 -0600

    pep

--- ui.py
+++ ui.py
@@ -1,6 +1,8 @@
-def display_menu_get_choice():
+""" coding=utf-8 """
 
-    '''Display choices for user, return users' selection'''
+
+def display_menu_get_choice():
+    """Display choices for user, return users' selection"""
 
     print('''
         1. Show unread books (wishlist)
@@ -22,7 +24,7 @@ def show_list(books):
     """ Format and display a list of book objects. """
 
     if len(books) == 0:
-        print ('* No books *')
+        print('* No books *')
         return
 
     for book in books:
@@ -33,11 +35,16 @@ def show_list(books):
 
 def get_title():
     """ Asks user for book title """
+
     book_title = input("Enter the Book Title ")
     return book_title
 
+
 def warn_title_read_previously(book):
-    print('System Shows the latest entry, {}, shares a title with a book that has already been marked as read. No changes to either have been made.'.format(book.title))
+    """ Printed when user adds a book that matches a title which has been read. """
+
+    print('System Shows the latest entry, {}, shares a title with a book that has already been marked as read.'.format(book.title))
+
 
 def ask_for_book_id():
     """ Ask user for book id, validate to ensure it is a positive integer """
@@ -45,10 +52,13 @@ def ask_for_book_id():
     while True:
         try:
             query_id = int(input('Enter book id:'))
+
             if query_id >= 0:
                 return query_id
+
             else:
                 print('Please enter a positive number ')
+
         except ValueError:
             print('Please enter an integer number')
 
@@ -64,19 +74,23 @@ def get_new_book_info():
 
 def message(msg):
     """ Display a message to the user """
+
     print(msg)
 
 
 def get_rating_info():
     """ Gets rating for a read book and ensures its within the acceptable range """
 
-    rating =input("Enter a rating between 1-5: ")
-    while rating not in str(range(1,6)):
+    rating = input("Enter a rating between 1-5: ")
+
+    while rating not in str(range(1, 6)):
         rating = input("Enter a number between 1 and 5 for the rating: ")
 
     return rating
 
+
 def get_read_update_type():
+    """ Gets user to specify if the boolean, book.read, should update to True or False"""
 
     while True:
         update_type = input('Enter 1 to mark as read. Enter 2 to mark as not read.')
@@ -84,6 +98,7 @@ def get_read_update_type():
         if update_type == '1':
             update_type = True
             break
+
         elif update_type == '2':
             update_type = False
             break
